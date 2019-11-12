e64a496ab025933eb271fbcf3cff6169e41ff08d
IMOKURI <nenegi.01mo@gmail.com>
2019-11-12 11:50:41 +0900

    update

--- web/home.py
+++ web/home.py
@@ -7,3 +7,8 @@ BP = Blueprint("home", __name__)
 @BP.route("/")
 def index():
     return render_template("pages/index.html")
+
+
+@BP.route("/ping")
+def ping():
+    return render_template("pages/ping.html")
